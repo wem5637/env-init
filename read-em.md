c45d8c0eeae3d695e197de178163879d703b9db8
Will McCracken <wem5637@gmail.com>
2023-11-27 15:37:02 -0600

    feat more

---
+++ read-em.md
@@ -0,0 +1,14 @@
+objectives:
+install an nvim environment
+edit the nvim config
+set up lsp
+
+reasonable aliases to use for grep
+use this as primary dev environment
+
+install git, set up profile
+
+install python, cargo, node, ruby on rails, alacritty
+
+future:
+extend to both linux and mac
--- setup.sh
+++ setup.sh
@@ -1,1 +1,28 @@
-echo "THIS IS A TEST TO RUN BASH SCRIPT"
+echo "THIS IS A SETUP SCRIPT"
+
+
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+apt-get install curl git npm nvm rbenv neovim tmux
+
+cat <<EOT >> .bashrc
+alias gb="git branch"
+alias gst="git status"
+alias gnb="git checkout -b"
+
+function F(){
+  grep $1 -iR $(git ls-files)
+}
+
+function P(){
+  list=$(git ls-files | grep $1 -i)
+  a=("${(f)list}")
+
+  select item in $a; do
+    echo "Opening file: $item"
+    lv $item
+    break
+  done
+}
+EOT
